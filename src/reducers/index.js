8014d4dc482dc7017ac2bda806cf89bc96b785d2
Johan <johan@paramation.com>
2019-02-08 14:58:24 +0700

    Add contractReducer and taoReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,14 @@ import { routerReducer } from "react-router-redux";
 import { web3Reducer } from "./web3Reducer";
 import { toastReducer } from "./toastReducer";
 import { appReducer } from "./appReducer";
+import { contractReducer } from "./contractReducer";
+import { taoReducer } from "./taoReducer";
 
 export const reducers = combineReducers({
 	web3Reducer,
 	toastReducer,
 	appReducer,
+	contractReducer,
+	taoReducer,
 	routing: routerReducer
 });
