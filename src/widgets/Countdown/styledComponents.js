4f852ceb07e89c82dad3164c38d6e4fa4ca3d068
Johan <johan@paramation.com>
2019-03-28 14:27:18 +0700

    Add Countdown widget

---
+++ src/widgets/Countdown/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const CountdownCol = styled.div`
+	display: inline-block;
+`;
+
+export const CountdownColEl = styled.div`
+	display: inline-block;
+	margin: 0 20px;
+	display: flex;
+	flex-direction: column;
+
+	> strong {
+		font-size: 2.5em;
+	}
+`;
