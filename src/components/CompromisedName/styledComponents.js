ebfa7b2c2da415026f0cc35241401d9c1536be97
Johan <johan@paramation.com>
2019-03-28 14:28:35 +0700

    Add CompromisedName component

---
+++ src/components/CompromisedName/styledComponents.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const WarningContainer = styled.div`
+	width: calc(100% - 80px);
+	height: 400px;
+	position: absolute;
+	top: 50%;
+	text-align: center;
+	margin: -200px 0 0 0;
+
+	> img {
+		width: 200px;
+	}
+`;
