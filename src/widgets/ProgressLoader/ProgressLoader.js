cf3fb2892e942ceecf8f37c34b795f593443f98a
Johan <johan@paramation.com>
2019-05-06 11:06:18 +0700

    Fix progress loader copy

--- src/widgets/ProgressLoader/ProgressLoader.js
+++ src/widgets/ProgressLoader/ProgressLoader.js
@@ -4,12 +4,9 @@ import { Wrapper } from "components/";
 class ProgressLoader extends React.Component {
 	render() {
 		const { pastEventsProgress } = this.props;
-		if (!pastEventsProgress) {
-			return null;
-		}
 		return (
 			<Wrapper className="padding-40">
-				Loading {pastEventsProgress === 100 ? "" : "initial data " + pastEventsProgress + "%"} ...
+				Loading {pastEventsProgress === 100 || !pastEventsProgress ? "" : "initial data " + pastEventsProgress + "%"} ...
 			</Wrapper>
 		);
 	}
